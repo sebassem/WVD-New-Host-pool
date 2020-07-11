67f5237c48293e7cb19fc50b5da8205605c836cd
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2020-07-11 14:17:40 +0200

    Delete New-WVDhostpool.ps1

--- New-WVDhostpool.ps1
+++
@@ -1,8 +0,0 @@
-New-AzWvdHostPool `
- -ResourceGroupName "RG-WVD-NewHostPool" `
- -Name "WVD-Hostpool1" `
- -WorkspaceName "WVD-Workspace1" `
- -HostPoolType "Pooled" `
- -LoadBalancerType "BreadthFirst" `
- -Location eastus `
- -DesktopAppGroupName "WVD-AppGroup1"
