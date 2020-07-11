0b4610847aa0cb03381b5f80d2e902c5394ccbca
seifbassem <sebassem@microsoft.com>
2020-07-11 15:42:15 +0200

    Added lines

--- New-WVDHostPool.ps1
+++ New-WVDHostPool.ps1
@@ -1,26 +1,8 @@
 
-# //TODO #4 Add the ability to accept parameters @sebassem
+
 
 ## Connect to Azure
 connect-azaccount
-
-##Initialize parameters
-param(
-[Parameter(Mandatory=$true)]
-[STRING]$resourcegroupname,
-[Parameter(Mandatory=$true)]
-[STRING]$location,
-[Parameter(Mandatory=$true)]
-[STRING]$hostPoolName,
-[Parameter(Mandatory=$true)]
-[STRING]$workspaceName,
-[Parameter(Mandatory=$true)]
-[STRING]$AppGroupName,
-[Parameter(Mandatory=$true)]
-[STRING]$HostPoolType,
-[Parameter(Mandatory=$true)]
-[STRING]$LoadBalancingType
-)
 
 ## Create a new HostPool
 New-AzWvdHostPool `
