d1448542def370d4b2dd0296e885cec7977acaa0
seifbassem <sebassem@microsoft.com>
2020-07-11 14:37:33 +0200

    Added a new file to create a hostpool

---
+++ New-WVDHostPool.ps1
@@ -0,0 +1,11 @@
+## Connect to Azure
+connect-azaccount
+## Create a new HostPool
+New-AzWvdHostPool `
+ -ResourceGroupName "RG-WVD-NewHostPool" `
+ -Name "WVD-Hostpool1" `
+ -WorkspaceName "WVD-Workspace1" `
+ -HostPoolType "Pooled" `
+ -LoadBalancerType "BreadthFirst" `
+ -Location eastus `
+ -DesktopAppGroupName "WVD-AppGroup1"
